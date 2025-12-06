09abec82bdae06f3fe0f545f1c6b47f9b5f0f6bd
adelaider <117357197+Adelaidaer@users.noreply.github.com>
2025-12-06 07:37:38 +0300

    final

---
+++ src/main/java/org/kursach/kursach/repository/ClientRepository.java
@@ -0,0 +1,17 @@
+package org.kursach.kursach.repository;
+
+import org.kursach.kursach.model.Client;
+import java.util.List;
+
+/**
+ * Интерфейс репозитория для работы с клиентами
+ */
+public interface ClientRepository extends Repository<Client, Long> {
+    
+    /**
+     * Найти клиентов по имени (поиск по части имени)
+     * @param name часть имени для поиска
+     * @return список найденных клиентов
+     */
+    List<Client> findByName(String name);
+}
